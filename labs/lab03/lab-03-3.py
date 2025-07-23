9acc08c69c2122bf4a07dd99dea2352b8b60f196
lionelptrick <EMAIL>
2025-07-23 22:01:53 +0800

    some changes

--- labs/lab03/lab-03-3.py
+++ labs/lab03/lab-03-3.py
@@ -1,8 +1,8 @@
-sizeKilobyte = float(input())
-megabyte = sizeKilobyte / 1024
-gigabyte = sizeKilobyte / 1024 * 1024
-terabyte = sizeKilobyte / 1024 * 1024 * 1024
-petabyte = sizeKilobyte / 1024 * 1024 * 1024 * 1024
+kilobyte = float(input())
+megabyte = kilobyte / 1024
+gigabyte = kilobyte / 1024 * 1024
+terabyte = kilobyte / 1024 * 1024 * 1024
+petabyte = kilobyte / 1024 * 1024 * 1024 * 1024
 print(megabyte)
 print(gigabyte)
 print(terabyte)
