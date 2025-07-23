3655d95166850e5bffb281b252039eabc9b7f4e5
lionelptrick <EMAIL>
2025-07-23 22:08:59 +0800

    formula changes

--- labs/lab03/lab-03-3.py
+++ labs/lab03/lab-03-3.py
@@ -1,8 +1,8 @@
 kilobyte = float(input())
-megabyte = kilobyte / 1024
-gigabyte = kilobyte / 1024 * 1024
-terabyte = kilobyte / 1024 * 1024 * 1024
-petabyte = kilobyte / 1024 * 1024 * 1024 * 1024
+megabyte = kilobyte / (1024)
+gigabyte = kilobyte / (1024 * 1024)
+terabyte = kilobyte / (1024 * 1024 * 1024)
+petabyte = kilobyte / (1024 * 1024 * 1024 * 1024)
 print(megabyte)
 print(gigabyte)
 print(terabyte)
