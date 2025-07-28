c422eeeaa00c9b6feeba6307f76d1d0e95c68b1c
lionelptrick <EMAIL>
2025-07-28 21:49:28 +0800

    fix coding

--- labs/lab03/lab-03-6.py
+++ labs/lab03/lab-03-6.py
@@ -1,6 +1,7 @@
-yardLength = float(input())
-yardWidth = float(input())
-houseLength = float(input())
-houseWidth = float(input())
+inputs = input().split()
+yardLength = float(input[0])
+yardWidth = float(input[1])
+houseLength = float(input[2])
+houseWidth = float(input[3])
 wage = (yardLength * yardWidth - houseLength * houseWidth) * 2
 print(wage)
