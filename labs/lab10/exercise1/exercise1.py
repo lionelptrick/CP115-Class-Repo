b53d0c991cb7c6ab548d4b35177f0a82688516ba
lionelptrick <EMAIL>
2025-09-30 11:20:44 +0800

    today lab 11

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -2,7 +2,25 @@ position = input()
 overtime_hours = int(input())
 is_weekend = input()
 
-# Your code here
+# Determine hourly rate
+hourly_rate = 0
+
+if position == "Manager":
+    hourly_rate = 35
+elif position == "Supervisor":
+    hourly_rate = 25
+elif position == "Staff":
+    hourly_rate = 18
+
+overtime_pay = 0
+total_pay = 0
+
+if is_weekend == "Yes":
+    overtime_pay = overtime_hours * hourly_rate * 1.5 + overtime_hours * 5
+else:
+    overtime_pay = overtime_hours * hourly_rate * 1.5
+
+total_pay = overtime_pay
 
 print(hourly_rate)
 print(overtime_pay)
--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -2,6 +2,30 @@ age = int(input())
 accident_count = int(input())
 
 # Your code here
+if age < 25:
+    base_premium = 2400
+elif 25 <= age <= 50:
+    base_premium = 1800
+else:
+    base_premium = 2000
+
+if accident_count == 0:
+    penalty = 0
+    discount_amount = base_premium * 0.1
+    final_premium = base_premium - discount_amount
+
+elif accident_count <= 2:
+    penalty = 300
+    final_premium = base_premium + penalty
+    discount_amount = 0
+
+else:
+    penalty = 600
+    final_premium = base_premium + penalty
+    discount_amount = 0
+
+final_premium = int(final_premium)
+discount_amount = int(discount_amount)
 
 print(base_premium)
 print(final_premium)
